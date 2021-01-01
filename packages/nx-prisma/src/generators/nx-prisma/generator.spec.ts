7c5b1e4ad2589e61c0c7098a752c4df64d1ae04a
Bram Borggreve <borggreve@gmail.com>
2021-01-01 02:41:45 -0500

    initial commit

---
+++ packages/nx-prisma/src/generators/nx-prisma/generator.spec.ts
@@ -0,0 +1,20 @@
+import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
+import { Tree, readProjectConfiguration } from '@nrwl/devkit';
+
+import generator from './generator';
+import { NxPrismaGeneratorSchema } from './schema';
+
+describe('nx-prisma generator', () => {
+  let appTree: Tree;
+  const options: NxPrismaGeneratorSchema = { name: 'test' };
+
+  beforeEach(() => {
+    appTree = createTreeWithEmptyWorkspace();
+  });
+
+  it('should run successfully', async () => {
+    await generator(appTree, options);
+    const config = readProjectConfiguration(appTree, 'test');
+    expect(config).toBeDefined();
+  });
+});
---
+++ packages/nx-prisma/src/generators/nx-prisma/schema.d.ts
@@ -0,0 +1,5 @@
+export interface NxPrismaGeneratorSchema {
+  name: string;
+  tags?: string;
+  directory?: string;
+}
